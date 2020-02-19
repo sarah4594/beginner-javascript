3bd5cfce099d7c6f15053d4573408059ddd263c9
sarah4594 <sarah4594@gmail.com>
2020-02-19 14:28:19 -0600

    ✨Click out of modal
    see comment at bottom

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,44 +1,45 @@
-const cardButtons = document.querySelectorAll('.card button');
-const modalOuter = document.querySelector('.modal-outer');
-const modalInner = document.querySelector('.modal-inner');
+/* eslint-disable prettier/prettier */
+const cardButtons = document.querySelectorAll('.card button')
+const modalInner = document.querySelector('.modal-inner')
+const modalOuter = document.querySelector('.modal-outer')
 
 function handleCardButtonClick(event) {
-  const button = event.currentTarget;
-  const card = button.closest('.card');
-  // Grab the image src
-  const imgSrc = card.querySelector('img').src;
-  const desc = card.dataset.description;
-  const name = card.querySelector('h2').textContent;
-  // populate the modal with the new info
-  modalInner.innerHTML = `
-    <img width="600" height="600" src="${imgSrc.replace(
-      '200',
-      '600'
-    )}" alt="${name}"/>
-    <p>${desc}</p>
-  `;
-  // show the modal
-  modalOuter.classList.add('open');
+  const button = event.currentTarget
+  // like querySelectorAll but the closest
+  const card = button.closest('.card')
+  // Get image source
+  const imgSrc = card.querySelector('img').src
+  const desc = card.dataset.description
+  const name = card.querySelector('h2').textContent
+  // populate modal with info
+  modalInner.innerHTML = `<img src="${imgSrc.replace(
+    '200',
+    '600',
+  )}" alt="${name}">
+  <p>${desc}</p>`
+  // show modal
+  modalOuter.classList.add('open')
 }
 
-cardButtons.forEach(button =>
-  button.addEventListener('click', handleCardButtonClick)
-);
-
 function closeModal() {
-  modalOuter.classList.remove('open');
+  modalOuter.classList.remove('open')
 }
 
 modalOuter.addEventListener('click', function(event) {
-  const isOutside = !event.target.closest('.modal-inner');
+  const isOutside = !event.target.closest('.modal-inner')
   if (isOutside) {
-    closeModal();
+    closeModal()
   }
-});
+})
 
 window.addEventListener('keydown', event => {
-  console.log(event);
   if (event.key === 'Escape') {
-    closeModal();
+    closeModal()
   }
-});
+})
+
+cardButtons.forEach(button => {
+  button.addEventListener('click', handleCardButtonClick)
+})
+
+// Switch between??
