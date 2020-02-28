28cd8aa2f361ed90804f298b0ce212deba7ff879
sarah4594 <sarah4594@gmail.com>
2020-02-28 13:15:32 -0600

    ✨Scroll to Accept

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,25 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions')
+const watch = document.querySelector('.watch')
+const button = document.querySelector('.accept')
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  // when it's 100% on page
+  threshold: 1,
+})
+
+function obCallback(payload) {
+  console.log(payload[0])
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false
+    // stop observing
+    ob.unobserve(terms.lastElementChild)
+  }
+  // good for pictures and stuff
+  // else {
+  //   button.disabled = true
+  // }
+}
+
+// observing last paragraph
+ob.observe(terms.lastElementChild)
