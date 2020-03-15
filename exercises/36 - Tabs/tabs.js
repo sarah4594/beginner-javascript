11bf8da3d6ee58efc79920d683ef838f966922ca
sarah4594 <sarah4594@gmail.com>
2020-03-15 10:39:41 -0500

    ✨Tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,31 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs')
+const tabButtons = tabs.querySelectorAll('[role="tab"]')
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'))
+
+function handleTabClick(event) {
+  // hide all tab paenls
+  tabPanels.forEach(panel => (panel.hidden = true))
+  // mark all other tabs as unselected
+  // for aria - use setAttribute
+  // tabButtons.forEach(tab => (tab.ariaSelected = false))
+  tabButtons.forEach(tab => tab.setAttribute('aria-selected', false))
+  // mark clikced tab as selected
+  event.currentTarget.setAttribute('aria-selected', true)
+  // find associated tab panel and show it
+  const { id } = event.currentTarget
+
+  /* METHOD 1
+  const tabPanel = tabs.querySelector(`[aria-labelledby=${id}]`)
+  tabPanel.hidden = false
+}
+*/
+
+  // Method 2 - find in array of tab panels
+  // can't do find right now - tabPanels is a nodeList
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id,
+  )
+  tabPanel.hidden = false
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick))
